a5b8c234c3f0c91f8327a10a58e2932402cf8fd0
kimkihong1018 <rhflffk101809@gmail.com>
2024-07-16 17:45:43 +0900

    second commit

--- script/main.js
+++ script/main.js
@@ -1,30 +1,39 @@
-$(function(){
+$(function () {
     //메뉴
     $(".sub , .sub_bg").hide();
-    $(".main > li , .sub_bg").hover(function(){
+    $(".main > li , .sub_bg").hover(function () {
         $(".sub , .sub_bg").stop().slideDown();
-    },function(){
+    }, function () {
         $(".sub , .sub_bg").stop().slideUp();
-    })//hover
+    }) //hover
 
-    var n = 0; //0 1 2
 
-    setInterval(function(){
-        if(n == 2){
-            n = 0;
-        }else{
-            n++;
-        }
-        console.log(n);
+    setInterval(function () {
+        $(".left_move").animate({left : "-1200px" }, 500, function(){
+            $(".left_move").append($(".left_move li").first());
+            $(".left_move").css({left : "0"})
+        })
+    }, 2500)
 
-        $(".left_move").animate({left : n * (-1200) + "px" }, 500)
-    }, 3000)
 
-    $(".p_click").click(function(){
+    // var n = 0; //0 1 2
+
+    // setInterval(function(){
+    //     if(n == 2){
+    //         n = 0;
+    //     }else{
+    //         n++;
+    //     }
+    //     console.log(n);
+
+    //     $(".left_move").animate({left : n * (-1200) + "px" }, 500)
+    // }, 3000)
+
+    $(".p_click").click(function () {
         $(".popup , .modal").show();
     })
 
-    $(".close").click(function(){
+    $(".close").click(function () {
         $(".popup , .modal").hide();
     })
 
